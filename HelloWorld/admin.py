6762ef65c3590edf01effaa6711eb2aed09bb5e1
TeenaMonteiro <teena.mon11@gmail.com>
2015-05-30 11:49:27 +0530

    Update admin.py

--- HelloWorld/admin.py
+++ HelloWorld/admin.py
@@ -2,19 +2,12 @@ from django.contrib import admin
 from HelloWorld.models import Users
 
 
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> a98ff8aadb705486cbe532470d833d4a9f1747fb
 class listusers(admin.ModelAdmin):
     list_display = ('username',"user_create_date")
 
 
-<<<<<<< HEAD
-=======
 
->>>>>>> a98ff8aadb705486cbe532470d833d4a9f1747fb
 admin.site.register(Users,listusers)
 
 
