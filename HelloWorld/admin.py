2a69179674bbf241113bfcba492d4c481e82967c
TeenaMonteiro <teena.mon11@gmail.com>
2015-05-30 11:45:36 +0530

    A simple change

--- HelloWorld/admin.py
+++ HelloWorld/admin.py
@@ -2,20 +2,9 @@ from django.contrib import admin
 from HelloWorld.models import Users
 
 
-# 
-# class UsersInLine(admin.StackedInline):
-#   
-#     model=Users
-  
+
 class listusers(admin.ModelAdmin):
     list_display = ('username',"user_create_date")
-#     inlines = [UsersInLine]
-
-
-
-# class listusers(admin.ModelAdmin):
-# #     date_heirachy = "user_create_date"
-#     model=Users
 
 
 admin.site.register(Users,listusers)
--- HelloWorld/models.py
+++ HelloWorld/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Users(models.Model):
-    username = models.CharField(max_length=50, primary_key=True)
+    username = models.CharField(max_length=50,primary_key =True)
     user_create_date = models.DateTimeField(auto_now_add = True)
 
     def __unicode__(self):
