d2727e5779e601d206ea6603b0d475cd96e1aa08
TeenaMonteiro <teena.mon11@gmail.com>
2015-06-01 21:04:21 +0530

    simpel change

--- dependency.py
+++ dependency.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = "HelloWorld",
     version = "1.0",
-    install_requires = ['MySQL-python==1.2.3','django==1.5'],
+    install_requires = ['django==1.5'],
     
     # metadata for upload to PyPI
     author = "Teena Monteiro",
